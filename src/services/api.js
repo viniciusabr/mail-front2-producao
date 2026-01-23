fb9dacd28e25c75c6a1911d9c98a999b42289074
viniciusabr <vinicius.abreu@gmail.com>
2026-01-23 14:26:20 -0300

    ajuste no token

--- src/services/api.js
+++ src/services/api.js
@@ -17,4 +17,15 @@ const api = axios.create({
   baseURL: "https://independent-vibrancy-production.up.railway.app",
 });
 
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
+
 export default api;
+
